e6c29ee426d5320ff22af5c490b87edcf40054ba
Alexandre Zampronne Zaccaron Rocha <106710530+AlexandreComn@users.noreply.github.com>
2023-08-27 13:28:53 -0300

    ok

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const content = document.querySelector('.content');
 sidebar.addEventListener('mouseover', () => {
     sidebar.classList.add('expanded');
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var cordelDiv = document.getElementById("cordel");
+    
+    cordelDiv.addEventListener("click", function() {
+      
+      var url = "https://alexandrecomn.github.io/Projeto-Cordel/ ";
+      window.location.href = url;
+    });
+  });
